7db6d4bfb4a181190331b473682e05c4a8288348
marthaan <marthaann27@gmail.com>
2024-11-22 19:15:47 -0800

    Comments and method headers added

--- myEncoder.java
+++ myEncoder.java
@@ -2,50 +2,42 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class myEncoder {
-    private static File inputFile;
+public class MyEncoder {
+    private File inputFile; // input file for each instance 
+    private int n1;  // foreground quantization step
+    private int n2;  // background quantization step
+
+    private static final int WIDTH = 960;   // width of each frame
+    private static final int HEIGHT = 540;  // height of each frame
+    private static final int channelSize = WIDTH * HEIGHT;  // 518,400 bytes per channel (per frame)
+    private static final int frameSize = channelSize * 3;   // 1,555,200 total bytes per frame
     
-    private static int n1; // foreground quantization step
-    private static int n2; // background quantization step
-    private static int width = 960;
-    private static int height = 540;
-    private static int channelSize = width * height;
-    private static int frameSize = channelSize * 3;
-    private static int[] prevFrame = new int[frameSize];
-    private static int[] currFrame = new int[frameSize];
-    
-    // PART1: videoSegmentation
+    private int[] prevFrame;
+    private int[] currFrame;
 
-    // getIFrame
-    // macroblock --> divide frames into 16 x 16 blocks
-    // computerMotionVector --> technique = MAD
-    // getLayers
+    /**
+     * Constructor
+     * @param inputFile
+     * @param n1
+     * @param n2
+     */
+    public MyEncoder(File inputFile, int n1, int n2) {
+        this.inputFile = inputFile; 
+        this.n1 = n1;
+        this.n2 = n2;
 
-
-    // PART2: compression
-    // block --> divide each macroblock into 8x8 blocks for each frame
-    // DCT
-    // quantize(n1, n2)
-    // scan
-
-    public static void main(String[]args) {
-        inputFile = new File(args[0]);
-        n1 = Integer.parseInt(args[1]);
-        n2 = Integer.parseInt(args[2]);
-
-        readFile(inputFile);
-
-        // .rgb folder --> video files = 960 x 540 (1/2 HD)
-            // 30 fps
-            // varied lengths --> read frame by frame until none left 
-        // wavs folder --> mp4 audio files
-            // 44.1 KHz
-
-
-        // open file
+        prevFrame = new int[frameSize];
+        currFrame = new int[frameSize];
     }
+    
 
-    public static void readFile(File inputFile) {
+    // PART 0: PRE-PROCESS FILE DATA
+    
+    /**
+     * Reads the entire .rgb file frame-by-frame
+     * Processes each frame based on frame type (I-frame vs. P-frame)
+     */
+    public void readFile() {
         try {
             FileInputStream fis = new FileInputStream(inputFile);
 
@@ -53,15 +45,15 @@ public class myEncoder {
                 formatFrame();
                 // if not I-frame
                 if (i != 0) {
-                    // PART1: videoSegmentation
-                    // macroblock --> divide frames into 16 x 16 blocks
-                    // computerMotionVector --> technique = MAD
-                    // getLayers
+                    // PART 1: VIDEO SEGMENTATION
+                    // macroblock();
+                    // computerMotionVector();
+                    // getLayer();
 
-                    // PART2: compression
-                    // block --> divide each macroblock into 8x8 blocks for each frame
-                    // DCT
-                    // quantize(n1, n2)
+                    // PART 2: COMPRESSION
+                    // block();
+                    // DCT();
+                    // quantize(n1, n2);
                     // write to compressed file
                     // store in prevFrame
                 }
@@ -79,7 +71,7 @@ public class myEncoder {
 
             // DEBUG: prints last 90 RGB values of last frame
             for (int i = 0; i < 30; i += 3) {
-                System.out.printf("R: %d, G: %d, B: %d\n", currFrame[i], currFrame[i+1], currFrame[i+2]);
+                System.out.printf("R: %d, G: %d, B: %d\n", currFrame[i], currFrame[i + 1], currFrame[i + 2]);
             }
 
             fis.close();
@@ -88,11 +80,18 @@ public class myEncoder {
         }
     }
 
-    public static boolean readFrame(FileInputStream fis) throws IOException {
+    /**
+     * Reads the bytes of a single frame into currFrame object
+     * @param fis input file stream for the .rgb file
+     * @return true if the current frame is read successfully, false if EOF reached
+     * @throws IOException if there is an error reading from the file
+     */
+    private boolean readFrame(FileInputStream fis) throws IOException {
         for (int i = 0; i < frameSize; i++) {
             // used temp variable so that final currFrame value is not assigned -1
-            int temp = 0;
-            if ((temp = fis.read()) == -1) {
+            int temp = fis.read();
+
+            if (temp == -1) {
                 return false;
             }
             else {
@@ -102,16 +101,64 @@ public class myEncoder {
         return true;
     }
 
-    public static void formatFrame() {
+    /**
+     * Rearranges the current frame's pixel data from RRR.GGG.BBB to RGB.RGB.RGB
+     */
+    private void formatFrame() {
         int[] tempArray = new int[frameSize];
 
-        // change currFrame from RRR.GGG.BBB to RGB.RGB.RGB
         for (int i = 0; i < channelSize; i++) {
-            tempArray[i*3] = currFrame[i];
-            tempArray[i*3+1] = currFrame[channelSize + i];
-            tempArray[i*3+2] = currFrame[channelSize * 2 + i];
+            tempArray[i * 3] = currFrame[i];
+            tempArray[i * 3 + 1] = currFrame[channelSize + i];
+            tempArray[i * 3 + 2] = currFrame[channelSize * 2 + i];
         }
 
         currFrame = tempArray;
+    }
+
+    // PART 1: VIDEO SEGMENTATION
+
+    // divide current frame into 16 x 16 blocks
+    private void macroblock() {
+        
+    }
+    
+    // technique to use = MAD
+    private void computeMotionVector() {
+
+    }
+
+    private void getLayer() {
+
+    }
+
+
+    // PART 2: COMPRESSION
+
+    // divide each macroblock into 8x8 blocks for each frame
+    private void block() {
+
+    }
+
+    private void dct() {
+
+    }
+
+    private void quantize() {
+
+    }
+
+    private void scan() {
+
+    }
+
+    public static void main(String[]args) {
+        File inputFile = new File(args[0]);
+        int n1 = Integer.parseInt(args[1]);
+        int n2 = Integer.parseInt(args[2]);
+
+        MyEncoder encoder = new MyEncoder(inputFile, n1, n2);
+
+        encoder.readFile();
     }
 }
