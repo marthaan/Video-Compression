3c57e9a1add4856b386fbba78b528b70a0d757b8
glehrhoff <glehrhof@usc.edu>
2024-12-04 02:19:12 -0800

    Fixed motionVectors

--- MyEncoder.java
+++ MyEncoder.java
@@ -4,6 +4,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -67,7 +68,6 @@ public class MyEncoder {
             FileInputStream fis = new FileInputStream(inputFile);
 
             for (int i = 0; readFrame(fis); i++) {
-                formatFrame();
 
                 // if not I-frame --> if P-frame
                 if (i != 0) {
@@ -115,31 +115,21 @@ public class MyEncoder {
     }
 
     /**
-     * Rearranges the current frame's pixel data from RRR.GGG.BBB to RGB.RGB.RGB
-     */
-    private void formatFrame() {
-        int[] tempArray = new int[FRAME_SIZE];
-
-        for (int i = 0; i < CHANNEL_SIZE; i++) {
-            tempArray[i * 3] = currFrame[i];
-            tempArray[i * 3 + 1] = currFrame[CHANNEL_SIZE + i];
-            tempArray[i * 3 + 2] = currFrame[CHANNEL_SIZE * 2 + i];
-        }
-
-        currFrame = tempArray;
-    }
-
-    /**
      * Handles video segmentation and compression for an I-frame
      */
     private void processIFrame() {
         // PART 1: VIDEO SEGMENTATION
         // goal: macroblocks[i] has motion vector at motionVectors[i] and has layer type at layers[i]
         currMacroblocks = macroblock(); // still macroblock so compression steps can be the same
-        
+
+        // all layers are quantized with foreground step
+        for (int i = 0; i < currMacroblocks.size(); i++) {
+            layers.add(0);
+        }
         
         // PART 2: COMPRESSION
         compress(currMacroblocks);
+
 
         // store in prevFrame:
         prevFrame3DArray = currFrame3DArray;
@@ -154,6 +144,18 @@ public class MyEncoder {
         currMacroblocks = macroblock();             
         motionVectors = generateMotionVectorArray(currMacroblocks);
         layers = getLayers();
+
+        // DEBUG: print out layers array
+        int numMacroblocksWide = (int)Math.ceil(WIDTH / (double)MACROBLOCK_SIZE);
+        int numMacroblocksHigh = (int)Math.ceil(HEIGHT / (double)MACROBLOCK_SIZE);
+        for (int y = 0; y < numMacroblocksHigh; y++) {
+            for (int x = 0; x < numMacroblocksWide; x++) {
+                int index = y * numMacroblocksWide + x;
+                System.out.print(layers.get(index));
+                // System.out.print("(" + motionVectors.get(index)[0] + " " + motionVectors.get(index)[1] + ")");
+            }
+            System.out.println();
+        }
 
         // PART 2: COMPRESSION
         compress(currMacroblocks);
@@ -187,9 +189,15 @@ public class MyEncoder {
 
         currFrame3DArray = convertTo3DArray(currFrame);
 
+        try {
+            MacroblockViewer.saveMacroblock(currFrame3DArray, "macroblock.png");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         // iterate over frame macroblock-by-macroblock
-        for (int x = 0; x < WIDTH; x += MACROBLOCK_SIZE) {
-            for (int y = 0; y < HEIGHT; y += MACROBLOCK_SIZE) {
+        for (int y = 0; y < HEIGHT; y += MACROBLOCK_SIZE) {
+            for (int x = 0; x < WIDTH; x += MACROBLOCK_SIZE) {
                 // create the current 16x16 macroblock to hold RGB channel data
                 int[][][] macroblock = new int[MACROBLOCK_SIZE][MACROBLOCK_SIZE][3];
                 
@@ -221,10 +229,10 @@ public class MyEncoder {
     private int[][][] convertTo3DArray(int[] frame) {
         int[][][] newFrame = new int[WIDTH][HEIGHT][3];
 
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
                 for (int channel = 0; channel < 3; channel++) {
-                    newFrame[x][y][channel] = frame[x*3 + y*3*WIDTH + channel];
+                    newFrame[x][y][channel] = frame[(y * WIDTH + x) * 3 + channel];
                 }
             }
         }
@@ -237,6 +245,7 @@ public class MyEncoder {
     // will need current macroblocks and prevFrame
     private List<int[]> generateMotionVectorArray(List<int[][][]> macroblocks) {
         // goal: macroblocks[i] has motion vector at motionVectors[i]
+        motionVectors = new ArrayList<>();
 
         // for each macroblock
         // compute that macroblock's motion vector and add to motionVectors
@@ -259,43 +268,43 @@ public class MyEncoder {
 
         int[] vector = new int[2]; // int[0] = dx, int[1] = dy
 
-        int topLeftX = i * MACROBLOCK_SIZE % WIDTH;
-        int topLeftY = i * MACROBLOCK_SIZE / WIDTH * MACROBLOCK_SIZE;
+        // ALTERNATE
+        // int topLeftX = i * MACROBLOCK_SIZE % WIDTH;
+        // int topLeftY = i * MACROBLOCK_SIZE / WIDTH * MACROBLOCK_SIZE;
+
+        int blocksPerRow = WIDTH / MACROBLOCK_SIZE;
+        int topLeftX = (i % blocksPerRow) * MACROBLOCK_SIZE;
+        int topLeftY = (i / blocksPerRow) * MACROBLOCK_SIZE;
+
 
         int minDifference = Integer.MAX_VALUE;
         
+        // search k to the left, but not less than 0
+        int xMin = Math.max(0, topLeftX - SEARCH_PARAMETER_K);
+        // search k to the right, but not more than WIDTH - MACROBLOCK_SIZE
+        int xMax = Math.min(WIDTH - MACROBLOCK_SIZE, topLeftX + SEARCH_PARAMETER_K);
+        // search k up, but not less than 0
+        int yMin = Math.max(0, topLeftY - SEARCH_PARAMETER_K);
+        // search k down, but not more than HEIGHT - height of last macroblock
+        int yMax = Math.min(HEIGHT / MACROBLOCK_SIZE * MACROBLOCK_SIZE, topLeftY + SEARCH_PARAMETER_K);
 
-        // search from k to the left to k to the right
-        for (int x = topLeftX - SEARCH_PARAMETER_K; x < topLeftX + SEARCH_PARAMETER_K; x++) {
-            // dealing with boundaries
-            while (x < 0) {
-                x++;
-            }
-            if (x > WIDTH - MACROBLOCK_SIZE) {
-                break;
-            }
-            // search from k up to k down
-            for (int y = topLeftY - SEARCH_PARAMETER_K; y < topLeftY + SEARCH_PARAMETER_K; y++) {
-                // dealing with boundaries
-                while (y < 0) {
-                    y++;
-                }
-                // this number intentionally does not include the fractional macroblocks
-                int numWholeMacroblocksHigh = HEIGHT / MACROBLOCK_SIZE;
-                if (y > numWholeMacroblocksHigh * MACROBLOCK_SIZE) {
-                    break;
-                }
+        int currMacroblockHeight = 16;
+        // if we're dealing with a fractional macroblock
+        if (topLeftY > HEIGHT - MACROBLOCK_SIZE) {
+            currMacroblockHeight = HEIGHT - topLeftY;
+        }
 
-                // dealing with fractional macroblocks
-                int numMacroblocksWide = WIDTH / MACROBLOCK_SIZE;
-                // this number also does not include fractional macroblocks
-                int numWholeMacroblocks = numWholeMacroblocksHigh * numMacroblocksWide;
-                int currMacroblockHeight = MACROBLOCK_SIZE;
-                // if we're dealing with a fractional macroblock
-                if (i >= numWholeMacroblocks) {
-                    currMacroblockHeight = HEIGHT - (numWholeMacroblocksHigh * MACROBLOCK_SIZE);
-                }
+        // first, check if current macroblock hasn't moved or changed at all
+        int diff = compareMacroblocks(currMacroblocks.get(i), topLeftX, topLeftY, currMacroblockHeight);
+        if (diff == 0) {
+            vector[0] = 0;
+            vector[1] = 0;
+            return vector;
+        }
 
+
+        for (int y = yMin; y <= yMax; y++) {
+            for (int x = xMin; x <= xMax; x++) {
                 // compare macroblocks
                 // if this is the smallest difference we have found so far, save that motion vector in vector
                 int difference = compareMacroblocks(currMacroblocks.get(i), x, y, currMacroblockHeight);
@@ -307,6 +316,7 @@ public class MyEncoder {
                 }
             }
         }
+        
 
         return vector;
     }
@@ -323,8 +333,8 @@ public class MyEncoder {
 
         // find the absolute value of the difference between each r pixel in currMacroblock and its
         // corresponding r pixel in prevFrame3DArray
-        for (int x = 0; x < MACROBLOCK_SIZE; x++) {
-            for (int y = 0; y < currMacroblockHeight; y++) {
+        for (int y = 0; y < currMacroblockHeight; y++) {
+            for (int x = 0; x < MACROBLOCK_SIZE; x++) {
                 difference += Math.abs(currMacroblock[x][y][0] - prevFrame3DArray[topLeftX+x][topLeftY+y][0]);
             }
         }
@@ -339,12 +349,15 @@ public class MyEncoder {
 
         int[] mostCommonVector = findMostCommonVector(motionVectors);
 
+        // DEBUG
+        System.out.println("mostCommonVector = (" + mostCommonVector[0] + " " + mostCommonVector[1] + ")");
+
         for (int i = 0; i < motionVectors.size(); i++) {
             // if the current motionVector is within the allowed error range from the mostCommonVector
-            if (motionVectors.get(i)[0] < mostCommonVector[0] + ALLOWED_VECTOR_ERROR &&
-                motionVectors.get(i)[0] > mostCommonVector[0] - ALLOWED_VECTOR_ERROR &&
-                motionVectors.get(i)[1] < mostCommonVector[1] + ALLOWED_VECTOR_ERROR &&
-                motionVectors.get(i)[1] > mostCommonVector[1] - ALLOWED_VECTOR_ERROR) {
+            if (motionVectors.get(i)[0] <= mostCommonVector[0] + ALLOWED_VECTOR_ERROR &&
+                motionVectors.get(i)[0] >= mostCommonVector[0] - ALLOWED_VECTOR_ERROR &&
+                motionVectors.get(i)[1] <= mostCommonVector[1] + ALLOWED_VECTOR_ERROR &&
+                motionVectors.get(i)[1] >= mostCommonVector[1] - ALLOWED_VECTOR_ERROR) {
                     // macroblock i is a background block (layers[i] = 1)
                     layers.add(1);
             }
@@ -355,42 +368,77 @@ public class MyEncoder {
         }
 
         // now check layers to make sure foreground elements are contiguous
-        layers = checkContiguous(layers);
+       layers = checkContiguous(layers);
 
         return layers;
     }
 
     private List<Integer> checkContiguous(List<Integer> layers) {
-        for (int i = 0; i < layers.size(); i++) {
-            // ceil function accounts for fractional macroblocks, so 33.75 macroblocks becomes 34
-            int numMacroblocksWide = (int)Math.ceil(WIDTH / (double)MACROBLOCK_SIZE);
-            int numMacroblocksHigh = (int)Math.ceil(HEIGHT / (double)MACROBLOCK_SIZE);
-            
-            // if the macroblock above, below, left, and right are all background, then this macroblock is background
+        // ceil function accounts for fractional macroblocks, so 33.75 macroblocks becomes 34
+        int numMacroblocksWide = (int)Math.ceil(WIDTH / (double)MACROBLOCK_SIZE);
+        int numMacroblocksHigh = (int)Math.ceil(HEIGHT / (double)MACROBLOCK_SIZE);
+
+        for (int y = 0; y < numMacroblocksHigh; y++) {
+            for (int x = 0; x < numMacroblocksWide; x++) {
+                int index = y * numMacroblocksWide + x;
+                // if foreground block
+                if (layers.get(index) == 0) {
+                    boolean isForeground = false;
+                    // if the macroblock above, below, left, and right are all background, then this macroblock is background
+                    // so, if any macroblock around is foreground, this macroblock is foreground, else background
+                    // if macroblock above is foreground, isForeground = true;
+                    if ((y != 0) && (layers.get(index - numMacroblocksWide) == 0)) {
+                        isForeground = true;
+                    }
+                    // if macroblock to the left is foreground
+                    if ((x != 0) && (layers.get(index - 1) == 0)) {
+                        isForeground = true;
+                    }
+                    // if macroblock to the right is foreground
+                    if ((x != numMacroblocksWide - 1) && (layers.get(index + 1) == 0)) {
+                        isForeground = true;
+                    }
+                    // if macroblock below is foreground
+                    if ((y != numMacroblocksHigh - 1) && (layers.get(index + numMacroblocksWide) == 0)) {
+                        isForeground = true;
+                    }
+                    if (isForeground == false) {
+                        // set to background
+                        layers.set(index, 1);
+                    }
+                }
+            }
         }
+
         return layers;
     }
 
+
     private int[] findMostCommonVector(List<int[]> motionVectors) {
         // make a hashmap where (key = vector, value = frequency)
-        HashMap<int[], Integer> vectorFrequencies = new HashMap<>();
+        HashMap<List<Integer>, Integer> vectorFrequencies = new HashMap<>();
 
         for (int[] vector : motionVectors) {
             // if vector is new, make a new entry (vector, 1)
             // if vector already exists in map, add 1 to its value
-            vectorFrequencies.put(vector, vectorFrequencies.getOrDefault(vector, 0)+1);
+            List<Integer> key = Arrays.asList(vector[0], vector[1]);
+            vectorFrequencies.put(key, vectorFrequencies.getOrDefault(key, 0)+1);
         }
 
-        int[] mostCommon = new int[2];
+        List<Integer> mostCommon = Arrays.asList(0, 0);
         int maxFrequency = 0;
-        for (Map.Entry<int[],Integer> entry : vectorFrequencies.entrySet()) {
+        for (Map.Entry<List<Integer>,Integer> entry : vectorFrequencies.entrySet()) {
             if (entry.getValue() > maxFrequency) {
                 mostCommon = entry.getKey();
                 maxFrequency = entry.getValue();
             }
         }
 
-        return mostCommon;
+        int[] mostCommonAsVector = new int[2];
+        mostCommonAsVector[0] = mostCommon.get(0);
+        mostCommonAsVector[1] = mostCommon.get(1);
+
+        return mostCommonAsVector;
     }
 
 
@@ -425,8 +473,8 @@ public class MyEncoder {
                 for (int i = 0; i < BLOCK_SIZE; i++) {
                     for (int j = 0; j < BLOCK_SIZE; j++) {
                         block[i][j][0] = macroblock[x + i][y + j][0];
-                        block[i][j][0] = macroblock[x + i][y + j][1];
-                        block[i][j][0] = macroblock[x + i][y + j][2];
+                        block[i][j][1] = macroblock[x + i][y + j][1];
+                        block[i][j][2] = macroblock[x + i][y + j][2];
                     }
                 }
 
