31e53188d720c015bc09a712afa93e06346e432c
marthaan <marthaann27@gmail.com>
2024-12-06 00:46:46 -0800

    decoder skeleton made

--- MyDecoder.java
+++ MyDecoder.java
@@ -1,9 +1,48 @@
-public class MyDecoder {
-    // PART3: decompression 
-    // parse compressed input file --> get n1, n2, & coeffs. of each macroblock
-    // dequantize
+import java.io.File;
 
-    // PART4: decoding
+public class MyDecoder {
+    File encoderFile;
+    int n1;
+    int n2;
+    
+    // constructor
+    public MyDecoder(File encoderFile) {
+        this.encoderFile = encoderFile;
+
+        n1 = 0;
+        n2 = 0; 
+    }
+
+
+    // ----- PART3: decompression -----
+
+    // parse compressed input file --> get n1, n2, & coeffs. of each macroblock
+    private void parseFile() {
+
+    }
+
+    // dequantize
+    private void dequantize() {
+
+    }
+
+
+    // ----- PART4: decoding -----
+
     // IDCT
-    // display 
+    private void idct() {
+
+    }
+
+    // display
+    private void display() {
+
+    }
+
+    
+    public static void main(String[] args) {
+        File encoderFile = new File(args[0]);     // only input = MyEncoder output file 
+
+        MyDecoder decoder = new MyDecoder(encoderFile);
+    }
 }
