d98398a11a3875945a642b0c8f325bda8cac2120
marthaan <marthaann27@gmail.com>
2024-12-10 12:13:49 -0800

    javaFX initial setup

--- AudioVideoPlayer.java
+++ AudioVideoPlayer.java
@@ -1,6 +1,13 @@
-public class AudioVideoPlayer {
-    // javaFX
-    
-    // play video at frame rate w/ synchronized sound
+// import javafx.application.Application;
+
+
+// javaFX video player
+// play video at frame rate w/ synchronized sound
     // must be able to play, pause, and step thru video frames
+public class AudioVideoPlayer {
+    // testing build
+
+    
+    
+    
 }
