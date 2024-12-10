b43ad970c132888715e80f790016b94527723fff
marthaan <marthaann27@gmail.com>
2024-12-10 12:48:02 -0800

    renamed root folder

--- AudioVideoPlayer.java
+++ AudioVideoPlayer.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 
 
-// javaFX video player
+// javaFX video playe
 // play video at frame rate w/ synchronized sound
     // must be able to play, pause, and step thru video frames
 public class AudioVideoPlayer extends Application {
