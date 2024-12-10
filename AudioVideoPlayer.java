631001b9cd8966e30fc2358c7ae4807b46ee301d
marthaan <marthaann27@gmail.com>
2024-12-10 12:51:08 -0800

    new branch setup

--- AudioVideoPlayer.java
+++ AudioVideoPlayer.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 
 
-// javaFX video playe
+// javaFX video player
 // play video at frame rate w/ synchronized sound
     // must be able to play, pause, and step thru video frames
 public class AudioVideoPlayer extends Application {
